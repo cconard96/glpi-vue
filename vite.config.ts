86e833c7dbc77216060e9c7a17471f14503703a8
Curtis Conard <cconard96@gmail.com>
2026-03-10 11:12:05 -0400

    basic pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,7 +18,40 @@ export default defineConfig(({ mode }) => {
                 },
             },
         },
-        plugins: [vue(), vueDevTools(), tailwindcss()],
+        plugins: [
+            vue(), vueDevTools(), tailwindcss(),
+            VitePWA({
+                manifestFilename: "manifest.json",
+                manifest: {
+                    "name": "GLPI (Unofficial)",
+                    "short_name": "GLPI (Unofficial)",
+                    "icons": [
+                        {
+                            "src": "Icon-512.png",
+                            "sizes": "512x512",
+                            "type": "image/png"
+                        },
+                        {
+                            "src": "Icon-192.png",
+                            "sizes": "192x192",
+                            "type": "image/png"
+                        }
+                    ],
+                    "start_url": "/",
+                    "background_color": "#ffffff",
+                    "display": "standalone",
+                    "theme_color": "#000000"
+                },
+                registerType: "autoUpdate",
+                includeAssets: ["favicon.ico"],
+                injectManifest: {
+                    globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+                },
+                workbox: {
+                    globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+                }
+            }),
+        ],
         define: {
             'globalThis.__DEV__': mode === 'development',
         },
